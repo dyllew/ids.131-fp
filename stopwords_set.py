22a9580d516c17aefe8c2766db171c6e57119efd
Dylan Lewis <dylanlewis@beepboopbopmachine.local>
2021-04-29 00:19:22 -0400

    added more stopwords and made more tf-idf and frequency plots over time

--- stopwords_set.py
+++ stopwords_set.py
@@ -1,7 +1,7 @@
 corpus_specific_stopwords = set(
     ['climate', 'change', 'global', 'warming', 'im', 'say', 'he', 'thats', 'dont', 
-    'thing', 'like', 'going', 'get', 'one', 'make', 'talk', 'would', 'said', 'well', 
+    'thing', 'like', 'going', 'get', 'got', 'one', 'make', 'talk', 'would', 'said', 'well', 
     'could', 'back', 'want', 'lot', 'also', 'see', 'way', 'look',
     'look', 'much', 'come', 'go', 'saying', 'come', 'some', 'say', 'take', 'something',
-    'there', 'that', 'really', 'talking', 'think'
+    'there', 'theres', 'that', 'really', 'talking', 'think', 'theyre'
     ])
